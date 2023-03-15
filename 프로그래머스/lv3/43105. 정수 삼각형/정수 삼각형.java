8b17e4d17b81866730ab91ed004376e283b60546
Sanghyeok Hyun <gmelon24kr@gmail.com>
2023-03-16 01:28:03 +0900

    Update 정수 삼각형.java

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[][] triangle) {
         int maxLength = triangle[triangle.length - 1].length;
@@ -8,20 +6,20 @@ class Solution {
         // 경로의 최대 값 계산
         for(int x = 0 ; x < triangle.length ; x++) {
             for (int y = 0 ; y < triangle[x].length ; y++) {
-                int maxAbove = 0;
+                int currentMax = 0;
                 if (x != 0 && y == 0) {
-                    maxAbove = dp[x - 1][0];
+                    currentMax = dp[x - 1][0];
                 }
                 if (x != 0 && y != 0) {
-                    maxAbove = Math.max(dp[x - 1][y], dp[x - 1][y - 1]);
+                    currentMax = Math.max(dp[x - 1][y], dp[x - 1][y - 1]);
                 }
-                dp[x][y] = triangle[x][y] + maxAbove;
+                dp[x][y] = triangle[x][y] + currentMax;
             }
         }
         
         // 마지막 라인 중 최대 값 구하기
         int answer = Integer.MIN_VALUE;
-        for (int i = 0 ; i < triangle[maxLength - 1].length ; i++) {
+        for (int i = 0 ; i < dp[maxLength - 1].length ; i++) {
             answer = Math.max(answer, dp[maxLength - 1][i]);
         }
         
