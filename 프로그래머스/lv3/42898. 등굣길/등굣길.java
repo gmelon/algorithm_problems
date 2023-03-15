11b50db9523c1cc2ccb09b5ae0912d9f614072c6
Sanghyeok Hyun <gmelon24kr@gmail.com>
2023-03-16 00:23:14 +0900

    [level 3] Title: 등굣길, Time: 0.68 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.java
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.java
@@ -1,39 +1,33 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] answer = new int[m + 1][n + 1];        
+        int[][] answer = new int[m + 1][n + 1];
         
-        // 물에 잠긴 지역 별도 배열로 생성
-        int[][] processedPuddles = new int[m + 1][n + 1];
+        // 물에 잠긴 지역 처리
         for (int i = 0 ; i < puddles.length ; i++) {
-            processedPuddles[puddles[i][0]][puddles[i][1]] = -1;
+            answer[puddles[i][0]][puddles[i][1]] = -1;
         }
         
-        return search(m, n, m, n, answer, processedPuddles);
-    }
-    
-    public int search(int m, int n,
-                       int x, int y,
-                       int[][] answer, int[][] processedPuddles) {
-        // 초기 케이스
-        if (x == 1 && y == 1) {
-            return 1;
-        }
-        // 범위를 벗어나면 제외
-        if (x > m || x < 1 || y > n || y < 1) {
-            return 0;
-        }
-        // 물에 잠긴 지역은 제외
-        if (processedPuddles[x][y] == -1) 
-            return 0;
+        // 초기 좌표 값 설정
+        answer[1][1] = 1;
         
-        // 메모이제이션
-        if (answer[x][y] != 0) {
-            return answer[x][y];
+        // 순회하며 경우의 수 계산
+        for (int x = 1 ; x <= m ; x++) {
+            for (int y = 1 ; y <= n ; y++) {
+                // 물에 잠긴 지역 제외
+                if (answer[x][y] == -1) {
+                    answer[x][y] = 0;
+                    continue;
+                }
+                
+                // 나머지 정상 경로 계산
+                if (x != 1) {
+                    answer[x][y] += answer[x - 1][y] % 1_000_000_007;
+                }
+                if (y != 1) {
+                    answer[x][y] += answer[x][y - 1] % 1_000_000_007;
+                }
+            }
         }
-        
-        // 나머지 정상 케이스
-        return answer[x][y] = (search(m, n, x - 1, y, answer, processedPuddles) 
-                    + search(m, n, x, y - 1, answer, processedPuddles)) 
-            % 1_000_000_007;
+        return answer[m][n] % 1_000_000_007;
     }
 }
