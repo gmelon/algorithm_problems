c5f52682e0a87783c35f9b6803e92b89d3aeb664
Sanghyeok Hyun <gmelon24kr@gmail.com>
2023-03-22 00:12:30 +0900

    [level 2] Title: 타겟 넘버, Time: 0.50 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,6 +1,21 @@
 class Solution {
+    
+    static int answer = 0;
+    
     public int solution(int[] numbers, int target) {
-        int answer = 0;
+        dfs(numbers, 1, numbers[0], target);
+        dfs(numbers, 1, -numbers[0], target);
         return answer;
+    }
+    
+    public void dfs(final int[] numbers, int index, int sum, int target) {
+        if (index == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
+            return;
+        }
+        dfs(numbers, index + 1, sum + numbers[index], target);
+        dfs(numbers, index + 1, sum - numbers[index], target);
     }
 }
