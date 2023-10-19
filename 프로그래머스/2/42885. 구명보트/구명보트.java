4facfda8651c6920a11dfa030232dc8d1526654a
Sanghyeok Hyun <gmelon24kr@gmail.com>
2023-10-19 17:11:27 +0900

    [level 2] Title: 구명보트, Time: 9.88 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        int answer = 0;
+        for( ; left <= right ; right--) {
+            if (left != right && people[left] + people[right] <= limit) {
+                left++;
+            }
+            answer += 1;
+        }
+        
+        return answer;
+    }
+}
