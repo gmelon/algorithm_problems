8f5816c2491316555501f58d7bad334994a297f3
Sanghyeok Hyun <gmelon24kr@gmail.com>
2024-02-03 22:15:24 +0900

    [Silver II] Title: N과 M (9), Time: 968 ms, Memory: 44720 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    static int N;
+    static int M;
+
+    static Set<int[]> set = new TreeSet<>((int[] i1, int[] i2) -> {
+        for (int i = 0; i < i1.length; i++) {
+            if (i1[i] != i2[i]) {
+                return i1[i] - i2[i];
+            }
+        }
+        return 0; // 동일
+    });
+    static int[] numbers;
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+        visited = new boolean[N];
+        numbers = new int[M];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        perm(0);
+        for (int[] i : set) {
+            System.out.println(Arrays.stream(i).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
+        }
+    }
+
+    static void perm(int index) {
+        if (index == M) {
+            set.add(numbers.clone());
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            visited[i] = true;
+            numbers[index] = arr[i];
+            perm(index + 1);
+            visited[i] = false;
+        }
+    }
+}
