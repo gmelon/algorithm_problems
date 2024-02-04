e65f263caf88eff9be8208497632abebebe33924
Sanghyeok Hyun <gmelon24kr@gmail.com>
2024-02-04 16:59:12 +0900

    [Silver I] Title: RGB거리, Time: 84 ms, Memory: 12164 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -39,7 +41,10 @@ public class Main {
         for (int i = 0; i < 3; i++) {
             answer = Math.min(answer, dp[N - 1][i]);
         }
-        System.out.println(answer);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
     }
 
 }
