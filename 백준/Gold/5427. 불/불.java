794cd4ba4dac1d67b2e938fdaeff20cf976ffec4
Sanghyeok Hyun <gmelon24kr@gmail.com>
2024-02-25 01:13:24 +0900

    [Gold IV] Title: 불, Time: 500 ms, Memory: 92312 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -2,146 +2,135 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
-    static int M;
+    static int w, h, ans;
+    static char[][] map;
+    static Point start;
+    static int[] di = {-1, +1, 0, 0};
+    static int[] dj = {0, 0, +1, -1};
+    //    static boolean[][] possible;
+    static Deque<Point> fire;
 
-    static Queue<Position> fires;
-    static Queue<Position> 상근이들;
-    static boolean[][] firesVisited;
-    static boolean[][] 상근Visited;
+    static class Point {
 
-    static boolean exit = false;
+        int i, j;
 
-    static class Position {
-
-        int x, y;
-
-        public Position(int x, int y) {
-            this.x = x;
-            this.y = y;
+        public Point(int i, int j) {
+            super();
+            this.i = i;
+            this.j = j;
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int T = Integer.parseInt(br.readLine());
-        loop:
-        for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            M = Integer.parseInt(st.nextToken()); // 열
-            N = Integer.parseInt(st.nextToken()); // 행
+        int TC = Integer.parseInt(st.nextToken());
+        for (int test = 0; test < TC; test++) {
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+            map = new char[h][w];
+            fire = new ArrayDeque<>();
+            ans = 0;
 
-            fires = new ArrayDeque<>();
-            상근이들 = new ArrayDeque<>();
-            firesVisited = new boolean[N][M];
-            상근Visited = new boolean[N][M];
-            exit = false;
-            for (int i = 0; i < N; i++) {
-                String line = br.readLine();
-                for (int j = 0; j < M; j++) {
-                    char c = line.charAt(j);
-                    if (c == '.') {
-                        continue;
+            for (int i = 0; i < h; i++) {
+                String str = br.readLine();
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = str.charAt(j);
+                    if (map[i][j] == '@') {
+                        start = new Point(i, j);
                     }
-
-                    firesVisited[i][j] = true;
-                    상근Visited[i][j] = true;
-                    if (c == '*') {
-                        // 불
-                        fires.offer(new Position(i, j));
-                    } else if (c == '@') {
-                        // 상근
-                        상근이들.offer(new Position(i, j));
-                        firesVisited[i][j] = false;
+                    if (map[i][j] == '*') {
+                        fire.add(new Point(i, j)); //불 좌표 저장
                     }
                 }
             }
 
-            int step = 0;
-            while (true) {
-                step++;
-                // 불 이동
-                if (fireSpread() == 0 & 상근spread() == 0) {
-                    break;
-                }
+            escapeBuilding(start);
 
-                // 상근이 이동
-                if (exit) {
-                    sb.append(step).append("\n");
-                    continue loop;
-                }
-            }
-            sb.append("IMPOSSIBLE").append("\n");
-        }
-
-        System.out.println(sb);
-    }
-
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-
-    static int fireSpread() {
-        int size = fires.size();
-        int count = 0;
-
-        while (size-- > 0) {
-            Position current = fires.poll();
-
-            for (int d = 0; d < 4; d++) {
-                int nX = current.x + dx[d];
-                int nY = current.y + dy[d];
-
-                if (nX < 0 || nX >= N || nY < 0 || nY >= M || firesVisited[nX][nY]) {
-                    continue;
-                }
-
-                // 번지기
-                firesVisited[nX][nY] = true;
-                상근Visited[nX][nY] = true;
-                fires.offer(new Position(nX, nY));
-                count++;
+            if (ans == 0) {
+                System.out.println("IMPOSSIBLE");
+            } else {
+                System.out.println(ans);
             }
         }
 
-        return count;
     }
 
-    static int 상근spread() {
-        int size = 상근이들.size();
-        int count = 0;
+    static void escapeBuilding(Point s) {
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(s.i, s.j));
+        int cnt = 0;
 
-        while (size-- > 0) {
-            Position current = 상근이들.poll();
+        while (!q.isEmpty()) {
+            int size = q.size();
+            int fireSize = fire.size();
+            for (int f = 0; f < fireSize; f++) {
+                spreadFire(fire.pollFirst());
+            }
 
-            for (int d = 0; d < 4; d++) {
-                int nX = current.x + dx[d];
-                int nY = current.y + dy[d];
+            for (int si = 0; si < size; si++) {
+                Point now = q.poll();
 
-                if (nX < 0 || nX >= N || nY < 0 || nY >= M) {
-                    // 탈출
-                    count++;
-                    exit = true;
-                    continue;
+                for (int d = 0; d < 4; d++) {
+                    int newi = now.i + di[d];
+                    int newj = now.j + dj[d];
+
+                    if (isValid(newi, newj)) {
+                        ans = cnt + 1;
+                        return;
+                    }
+
+                    if (newi >= 0 && newj >= 0 && newi < h && newj < w && map[newi][newj] == '.') {
+                        map[newi][newj] = '@';
+                        q.add(new Point(newi, newj));
+                    }
                 }
+            }
+            cnt++;
+        }
+    }
 
-                if (상근Visited[nX][nY]) {
-                    continue;
-                }
+    static void spreadFire(Point f) {
+        int i = f.i;
+        int j = f.j;
+        for (int d = 0; d < 4; d++) {
+            int newi = i + di[d];
+            int newj = j + dj[d];
 
-                // 번지기
-                상근Visited[nX][nY] = true;
-                상근이들.offer(new Position(nX, nY));
-                count++;
+            if (newi >= 0 && newj >= 0 && newi < h && newj < w && map[newi][newj] != '#' && map[newi][newj] != '*') {
+                map[newi][newj] = '*';
+                fire.offerLast(new Point(newi, newj));
+
             }
         }
-
-        return count;
     }
+
+    static boolean isValid(int i, int j) {
+        if (i < 0 || j < 0 || i >= h || j >= w) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    static void printMap() {
+        for (int i = 0; i < h + 2; i++) {
+            for (int j = 0; j < w + 2; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("==============================");
+    }
+
 }
